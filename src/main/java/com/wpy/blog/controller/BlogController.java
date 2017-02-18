7a52a88f558e4daae254bc06dcee581ba348de23
wpy23tl <798841216@qq.com>
2017-02-18 18:45:00 +0800

    2.18

--- src/main/java/com/wpy/blog/controller/BlogController.java
+++ src/main/java/com/wpy/blog/controller/BlogController.java
@@ -10,9 +10,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.wpy.blog.entity.Link;
 import com.wpy.blog.service.LinkService;
-import com.wpy.blog.util.EhcacheUtil;
 import net.sf.ehcache.Cache;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -58,7 +56,7 @@ public class BlogController {
 	 * @param model
 	 * @return
 	 */
-	/*@RequestMapping("/index")
+	@RequestMapping("/index")
 	public String index(HttpServletRequest request,HttpServletResponse response,Model model,@RequestParam(value="page",required=false)String page,
 			@RequestParam(value="rows",required=false)String pageSize,String blogTypeId){
 		Map<String,Object> map = new HashMap<>();
@@ -125,98 +123,9 @@ public class BlogController {
 		String pageCode = PageUtil.genPageCode(totalCount, Integer.valueOf(pageSize),Integer.valueOf(page), blogTypeId, request);
 		model.addAttribute("pageCode",pageCode);
 		return "portal/index";
-	}*/
-
-	@RequestMapping("/index")
-	public String index(HttpServletRequest request,HttpServletResponse response,Model model,@RequestParam(value="page",required=false)String page,
-						@RequestParam(value="rows",required=false)String pageSize,String blogTypeId){
-		Map<String,Object> map = new HashMap<>();
-		//获取所有博客类型
-		if("".equals(page) || page==null){
-			page="1";
-		}
-		if("".equals(pageSize) || pageSize==null){
-			pageSize="10";
-		}
-
-		PageBean pageBean = new PageBean(Integer.valueOf(page),Integer.valueOf(pageSize));
-		map.put("start", pageBean.getStart());
-		map.put("size", pageBean.getPageSize());
-		if(blogTypeId!=null && !"null".equals(blogTypeId) ){
-			map.put("blogTypeId",Integer.valueOf(blogTypeId));
-		}
-
-		//添加缓存
-		EhcacheUtil ehcacheUtil = EhcacheUtil.getInstance();
-
-
-		if(cache==null){
-			cache=ehcacheUtil.get("userCache");
-			Map<String,Object> map0 = new HashMap<>();
-			 blogTypeList =blogTypeService.getCount(map0);
-			ehcacheUtil.put("userCache","blogTypeList",blogTypeList);
-			model.addAttribute("blogTypeList",blogTypeList);
-			//获取所有博客
-			List<Blog> blogList =  blogService.getAllBlog(map);
-			 newBlogList = new ArrayList<>();
-			for(Blog blog:blogList){
-				Date createTime = blog.getCreateTime();
-				String createTimeString = DateTimeUtil.DateToString(createTime, "yyyy-MM-dd HH:mm:ss");
-				BlogVo blogVo = new BlogVo();
-				BeanUtils.copyProperties(blog, blogVo);
-				blogVo.setCreateTime(createTimeString);
-				newBlogList.add(blogVo);
-			}
-
-
-			//获取所有banner
-			 bannerBlogList = blogService.getBanner();
-			ehcacheUtil.put("userCache","bannerBlogList",bannerBlogList);
-			model.addAttribute("bannerBlogList",bannerBlogList);
-			//获取博客总数
-			 totalCount = null;
-			if(blogTypeId==null || "".equals(blogTypeId) || "null".equals(blogTypeId)){
-				totalCount = blogService.getTotalCount();
-			}else{
-				Map<String,Object> map1 = new HashMap<>();
-				map1.put("id",blogTypeId);
-				List<BlogType> blogTypeList1 = blogTypeService.getCount(map1);
-				BlogType blogType = blogTypeList1.get(0);
-				totalCount= blogType.getBlogTypeCount();
-			}
-			for(BlogVo blog:newBlogList){
-				List<String> imagesList=blog.getImagesList();
-				String blogInfo=blog.getBlogContent();
-				Document doc=Jsoup.parse(blogInfo);
-				Elements jpgs=doc.select("img[src$=.jpg]"); //　查找扩展名是jpg的图片
-				for(int i=0;i<jpgs.size();i++){
-					if(i==1){
-						break;
-					}
-					Element jpg=jpgs.get(i);
-					imagesList.add(jpg.toString());
-
-				}
-			}
-			ehcacheUtil.put("userCache","blogList",newBlogList);
-			model.addAttribute("blogList",newBlogList);
-		}else{
-			blogTypeList =(List<BlogType>)ehcacheUtil.get("userCache","blogTypeList");
-			model.addAttribute("blogTypeList",blogTypeList);
-
-			bannerBlogList =(List<Blog>)ehcacheUtil.get("userCache","bannerBlogList");
-			model.addAttribute("bannerBlogList",bannerBlogList);
-
-			newBlogList  =(List<BlogVo>)ehcacheUtil.get("userCache","newBlogList");
-			model.addAttribute("blogList",newBlogList);
-
-		}
-
-
-		String pageCode = PageUtil.genPageCode(totalCount, Integer.valueOf(pageSize),Integer.valueOf(page), blogTypeId, request);
-		model.addAttribute("pageCode",pageCode);
-		return "portal/index";
 	}
+
+
 	/**
 	 * @author wpy
 	 * @description 进入文章
